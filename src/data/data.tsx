eadfaca15e5e1b66322e66d50411d455a3f9567b
Razvan Negrila <122675523+rnegrila@users.noreply.github.com>
2025-02-11 00:14:56 +0200

    update info

--- src/data/data.tsx
+++ src/data/data.tsx
@@ -11,7 +11,6 @@ import {
 import GithubIcon from '../components/Icon/GithubIcon';
 import InstagramIcon from '../components/Icon/InstagramIcon';
 import LinkedInIcon from '../components/Icon/LinkedInIcon';
-import StackOverflowIcon from '../components/Icon/StackOverflowIcon';
 import TwitterIcon from '../components/Icon/TwitterIcon';
 import heroImage from '../images/header-background.webp';
 import porfolioImage1 from '../images/portfolio/portfolio-1.jpg';
@@ -347,7 +346,7 @@ export const contact: ContactSection = {
     },
     {
       type: ContactType.Instagram,
-      text: '@tbakerx',
+      text: '@negrila.razvan.1',
       href: 'https://www.instagram.com/negrila.razvan.1/',
     },
     {
